4c0cb653a89407d4ccb59376bc0651e3e2b0a198
Загорулькин Дмитрий Юрьевич <zagor_dmtr@mail.ru>
2019-06-19 13:30:21 +0300

    Added hatch

---
+++ IterisCivilDynamo/AutoCAD/Hatch.cs
@@ -0,0 +1,33 @@
+﻿using DynamoServices;
+using AcDbHatch = Autodesk.AutoCAD.DatabaseServices.Hatch;
+using DynAcObject = Autodesk.AutoCAD.DynamoNodes.Object;
+
+namespace IterisCivilDynamo.AutoCAD
+{
+    /// <summary>
+    /// Dynamo representation of the AutoCAD Hatch object 
+    /// </summary>
+    [RegisterForTrace]
+    public class Hatch : DynAcObject
+    {
+        internal AcDbHatch DbHatch => AcObject as AcDbHatch;
+
+        internal Hatch(AcDbHatch hatch, bool isDynamoOwned = false)
+            : base(hatch, isDynamoOwned)
+        {
+        }
+
+        /// <summary>
+        /// The area of the hatch
+        /// </summary>
+        public double Area => DbHatch.Area;
+
+        /// <summary>
+        /// Get hatch from object
+        /// </summary>
+        /// <param name="dynAcObject"></param>
+        public Hatch(DynAcObject dynAcObject) : this(dynAcObject.InternalDBObject as AcDbHatch)
+        {            
+        }
+    }
+}
